5de6b189695c0ee3a710f3ea729ef9d556e22d3e
thiagohenriquesantos <thiagosantosengcivil@gmail.com>
2019-07-27 11:14:08 -0300

    update user configurated

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 
-import { decode } from 'punycode';
 import authConfig from '../../config/auth';
 
 export default async (req, res, next) => {
